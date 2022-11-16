5e7891dbd5fe8ef7194bcbf50466d03bf63ff588
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-11-16 17:09:27 -0500

    Add files via upload

---
+++ js/elements.js
@@ -0,0 +1,170 @@
+function onelement1() {
+  var x = document.getElementById("divResize1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function onelement2() {
+  var x = document.getElementById("divResize2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function onelement3() {
+  var x = document.getElementById("divResize3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement4() {
+  var x = document.getElementById("divResize4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement5() {
+  var x = document.getElementById("divResize5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement6() {
+  var x = document.getElementById("divResize6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement7() {
+  var x = document.getElementById("divResize7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement8() {
+  var x = document.getElementById("divResize8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement9() {
+  var x = document.getElementById("divResize9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement10() {
+  var x = document.getElementById("divResize10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement11() {
+  var x = document.getElementById("divResize11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement12() {
+  var x = document.getElementById("divResize12");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement13() {
+  var x = document.getElementById("divResize13");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement14() {
+  var x = document.getElementById("divResize14");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement15() {
+  var x = document.getElementById("divResize15");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement16() {
+  var x = document.getElementById("divResize16");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement17() {
+  var x = document.getElementById("divResize17");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement18() {
+  var x = document.getElementById("divResize18");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement19() {
+  var x = document.getElementById("divResize19");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement20() {
+  var x = document.getElementById("divResize20");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement21() {
+  var x = document.getElementById("divResize21");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
